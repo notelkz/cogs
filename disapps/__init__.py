3deb2207a71b820709266a4bccce952a09fdad9a
notelkz <matt.sw4@outlook.com>
2025-04-28 01:45:21 +0100

    Update __init__.py

--- disapps/__init__.py
+++ disapps/__init__.py
@@ -3,38 +3,3 @@ from .disapps import DisApps
 
 async def setup(bot: Red) -> None:
     await bot.add_cog(DisApps(bot))
-
-    # Initialize default guild settings
-    defaults = {
-        "mod_role": None,
-        "accepted_role": None,
-        "assignable_roles": [],
-        "applications_category": None,
-        "archive_category": None,
-        "setup_complete": False,
-        "applications": {}
-    }
-    
-    # Initialize Config
-    config = Config.get_conf(
-        cog,
-        identifier=1234567890,
-        force_registration=True
-    )
-    
-    # Register default guild settings
-    config.register_guild(**defaults)
-    
-    # Example of application data structure in applications dict:
-    # {
-    #     "user_id": {
-    #         "channel_id": 123456789,
-    #         "status": "pending/accepted/declined",
-    #         "declines": 0,
-    #         "timestamp": 1234567890,
-    #         "previously_accepted": False
-    #     }
-    # }
-    
-    await bot.add_cog(cog)
-
