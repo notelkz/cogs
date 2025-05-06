e71e1071013f16fd870e81c4bc3f1fbd3ae77de1
notelkz <matt.sw4@outlook.com>
2025-05-06 11:22:57 +0100

    Update __init__.py

--- usertracker/__init__.py
+++ usertracker/__init__.py
@@ -2,7 +2,11 @@ from .usertracker import UserTracker
 
 __red_end_user_data_statement__ = "This cog stores user voice time and message counts for tracking purposes."
 
-def setup(bot):
-    cog = UserTracker(bot)
-    bot.add_cog(cog)
-    bot.loop.create_task(cog.initialize())
+async def setup(bot):
+    try:
+        cog = UserTracker(bot)
+        await bot.add_cog(cog)  # Await the add_cog method
+        await cog.initialize()  # Await the initialize method
+        print("UserTracker cog loaded successfully.")
+    except Exception as e:
+        print(f"Failed to load UserTracker cog: {str(e)}")
