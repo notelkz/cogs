cbffcba43c9fdaf0ef918b2648568109b65b4a4e
notelkz <matt.sw4@outlook.com>
2025-05-06 11:02:24 +0100

    Update __init__.py

--- usertracker/__init__.py
+++ usertracker/__init__.py
@@ -2,5 +2,7 @@ from .usertracker import UserTracker
 
 __red_end_user_data_statement__ = "This cog stores user voice time and message counts for tracking purposes."
 
-async def setup(bot):
-    await bot.add_cog(UserTracker(bot))
+def setup(bot):
+    cog = UserTracker(bot)
+    bot.add_cog(cog)
+    bot.loop.create_task(cog.initialize())
