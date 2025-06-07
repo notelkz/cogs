617ebb30137a04267175ac6223ed374f1371ba06
notelkz <matt.sw4@outlook.com>
2025-06-08 00:59:22 +0100

    Update twitchschedule.py

--- twitchschedule/twitchschedule.py
+++ twitchschedule/twitchschedule.py
@@ -272,9 +272,9 @@ class TwitchSchedule(commands.Cog):
             day_x = 125    # X position for day/time
             game_x = 125   # X position for game title
             initial_y = 350    # Starting Y position for first day
-            row_height = 200   # Space between each row's start
+            row_height = 180   # Space between each row's start
             day_offset = -45   # How far above the bar the day text should be
-            bar_height = 80    # Height of the semi-transparent bar
+            bar_height = 70    # Height of the semi-transparent bar
 
             # Add schedule items
             for i, segment in enumerate(schedule):
