ce41530634ad31fca6e2f9c675ccf54e1450c4b9
notelkz <matt.sw4@outlook.com>
2025-07-13 02:14:21 +0100

    Update __init__.py

--- zerocalendar/__init__.py
+++ zerocalendar/__init__.py
@@ -1,1 +1,4 @@
-from .zerocalendar import setup
+def setup(bot):
+    # Import the class directly inside the setup function
+    from zerocalendar.zerocalendar import ZeroCalendar
+    bot.add_cog(ZeroCalendar(bot))
