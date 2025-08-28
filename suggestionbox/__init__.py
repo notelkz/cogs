fb67c47bfe8143e43067f6ec97036355ef226317
notelkz <matt.sw4@outlook.com>
2025-08-29 00:03:56 +0100

    New Cog

---
+++ suggestionbox/__init__.py
@@ -0,0 +1,12 @@
+from .suggestionbox import SuggestionBox
+
+__red_end_user_data_statement__ = (
+    "This cog stores suggestion content, timestamps, and status information. "
+    "No personal data is stored - suggestions are anonymous. "
+    "All data can be removed by deleting the guild's configuration."
+)
+
+
+async def setup(bot):
+    """Load the SuggestionBox cog."""
+    await bot.add_cog(SuggestionBox(bot))
