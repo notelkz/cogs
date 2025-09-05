094ceec34167b65bbe8b7b3745ca67b634da0cbf
notelkz <matt.sw4@outlook.com>
2025-09-05 15:56:30 +0100

    Update __init__.py

--- backlogged/__init__.py
+++ backlogged/__init__.py
@@ -2,4 +2,5 @@ from .twitchschedule import TwitchSchedule
 
 async def setup(bot):
     """Entry point for Red-DiscordBot cog loading"""
-    await bot.add_cog(TwitchSchedule(bot))
+    cog = TwitchSchedule(bot)
+    await bot.add_cog(cog)
